8becb548fc23b5c3ef160e4b283e588de3d4fe6c
Mohiuddin2000 <mohiuddinmohammad2000@gmail.com>
2022-09-30 18:51:23 +0600

    3rd commit

--- src/comonents/Blog/Blog.js
+++ src/comonents/Blog/Blog.js
@@ -18,15 +18,9 @@ const Blog = () => {
                         <div id="collapseOne" classNameName="accordion-collapse collapse show" aria-labelledby="headingOne"
                             data-bs-parent="#accordionExample">
                             <div className="accordion-body">
-                                <p>1.The scope of a var variable is functional scope.On the other hand,The scope of a
-                                    let and const variable is block scope.</p>
-                                <p>2.var can be updated and re-declared into the scope.let can be updated but cannot be
-                                    re-declared into the scope.On the other hand,const cannot be updated or re-declared
-                                    into the scope.</p>
-                                <p>3.var can be accessed without initialization as its default value is “undefined”.let
-                                    cannot be accessed without initialization otherwise it will give ‘referenceError’.On
-                                    the other hand,const cannot be accessed without initialization, as it cannot be
-                                    declared without initialization.</p>
+                                <p>While building client-side apps, a team of Facebook developers realized that the DOM is slow (The Document Object Model (DOM) is an application programming interface (API) for HTML and XML documents. It defines the logical structure of documents and the way a document is accessed and manipulated.). So, to make it faster, React implements a virtual DOM that is basically a DOM tree representation in JavaScript. So when it needs to read or write to the DOM, it will use the virtual representation of it. Then the virtual DOM will try to find the most efficient way to update the browser’s DOM.
+                                    Unlike browser DOM elements, React elements are plain objects and are cheap to create. React DOM takes care of updating the DOM to match the React elements. The reason for this is that JavaScript is very fast and it’s worth keeping a DOM tree in it to speed up its manipulation.
+                                    Although React was conceived to be used in the browser, because of its design it can also be used in the server with Node.js. </p>
                             </div>
                         </div>
                     </div>
